8df3e64a788155bdc344a658be105068dd78baea
Gil Tober <giltober@gmail.com>
2020-08-22 12:39:22 +0300

    exceptions

--- telegram_hook.py
+++ telegram_hook.py
@@ -1,18 +1,18 @@
-from airflow.hooks.base_hook import BaseHook
-from airflow.exceptions import AirflowException
 import telebot
+from airflow.exceptions import AirflowException
+from airflow.hooks.base_hook import BaseHook
 
 
 class TelegramHook(BaseHook):
     # TODO Docstring
 
-    def __init__(self, telegram_conn_id=None, chat_id=None, *args, **kwargs):
+    def __init__(self, telegram_conn_id=None, token=None, chat_id=None, *args, **kwargs):
         # TODO Docstring
-        super(TelegramHook, self).__init__(*args, **kwargs)
-        self.token = self._get_token(telegram_conn_id)
+        super().__init__(*args, **kwargs)
+        self.token = self._get_token(telegram_conn_id, token)
         self.chat_id = self._get_chat_id(telegram_conn_id, chat_id)
 
-    def _get_token(self, token, telegram_conn_id):
+    def _get_token(self, telegram_conn_id, token):
         # TODO Docstring
         if token:
             return token
@@ -20,8 +20,8 @@ class TelegramHook(BaseHook):
         elif telegram_conn_id:
             conn = self.get_connection(telegram_conn_id)
 
-            if not getattr(conn, 'password', None):
-                raise AirflowException(f'Token(password) is missing Telegram connection')
+            if not conn.password:
+                raise AirflowException('Token(password) is missing from telegram_conn_id')
 
             return conn.password
         else:
@@ -35,20 +35,17 @@ class TelegramHook(BaseHook):
         if telegram_conn_id:
             conn = self.get_connection(telegram_conn_id)
 
-            if not getattr(conn, 'host', None):
-                raise AirflowException('write something')
+            if not conn.host:
+                raise AirflowException('Chat_id(host) is missing from telegram_conn_id')
 
             else:
                 return conn.host
 
         else:
-            raise AirflowException('write')
+            raise AirflowException(
+                "Cannot get chat_id: No valid chat_id nor telegram_conn_id with 'host' field was given")
 
     def send_message(self, message):
         # TODO Docstring
-        try:
-            bot = telebot.TeleBot(token=self.token)
-        except Exception as e:
-            pass
-        else:
-            bot.send_message(chat_id=self.chat_id, text=message)
+        telegram_client = telebot.TeleBot(token=self.token)
+        telegram_client.send_message(chat_id=self.chat_id, text=message)
