1434e697bfbf47297daef67716b8ab785fd5ef09
Gil Tober <giltober@gmail.com>
2020-08-23 15:48:05 +0300

    fixes

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from airflow.plugins_manager import AirflowPlugin
 
-from plugins.airflow_telegram_plugin.hooks.telegram_hook import TelegramHook
-from plugins.airflow_telegram_plugin.operators.telegram_operator import TelegramOperator
+from airflow_telegram_plugin.hooks.telegram_hook import TelegramHook
+from airflow_telegram_plugin.perators.telegram_operator import TelegramOperator
 
 
 class TelegramPlugin(AirflowPlugin):
