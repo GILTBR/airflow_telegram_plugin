77fe6127867b909c8131d65e840aeaad56392d5a
Gil Tober <giltober@gmail.com>
2020-08-26 16:56:35 +0300

    parse_mode

--- hooks/telegram_hook.py
+++ hooks/telegram_hook.py
@@ -76,9 +76,9 @@ class TelegramHook(BaseHook):
 
     def send_message(self, message):
         # TODO Docstring
-        telegram_client = telebot.TeleBot(token=self.token)
+        telegram_client = telebot.TeleBot(token=self.token, parse_mode='Markdown')
         self.log.info(f'Sending message: {message}')
         try:
-            telegram_client.send_message(chat_id=self.chat_id, text=message, parse_mode='Markdown')
+            telegram_client.send_message(chat_id=self.chat_id, text=message)
         except AirflowException as e:
             self.log.exception(e)
